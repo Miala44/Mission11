1410cf15503e83ee469efb82b7171aac239be51e
Miala44 <adamsmia7@gmail.com>
2025-04-04 12:18:22 -0600

    about to deploy the frontend

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6e1ce9e2c6a5efed919c41627414535dc3ad42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fea99a7b52b9817ae1740b2618c6a5e87ab26fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
